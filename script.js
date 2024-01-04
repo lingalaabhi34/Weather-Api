d5373cf621c35d8210756367b42abd0f3a0b25b9
lingalaabhi34 <lingalaabhi34@gmail.com>
2024-01-04 19:37:04 +0530

    Weather-Api

--- script.js
+++ script.js
@@ -1,114 +1,98 @@
 
 const form = document.getElementById("input");
-const submitbutton =document.getElementById("submitbutton");
-const msg = document.querySelector(".form-msg");
-const input = document.getElementById("input-data");
-const list = document.querySelector(".cities")
-const apiKey = 'eba6f122079354c1574c61520d9de148';
-
-
-form.addEventListener('submit', e => {
-	// Prevent default form submission
-	e.preventDefault()
-
-	// Hide any message that might be displayed
-	msg.textContent = ''
-	msg.classList.remove('visible')
-
-	// Get the search value
-	let inputVal = input.value
-
-	// Check if there's already a city that matches the search criteria
-	const listItemsArray = Array.from(list.querySelectorAll('.cities li'))
-
-	if (listItemsArray.length > 0) {
-		const filteredArray = listItemsArray.filter(el => {
-			let content = ''
-			let cityName = el.querySelector('.city__name').textContent.toLowerCase()
-			let cityCountry = el.querySelector('.city__country').textContent.toLowerCase()
-
-			// Check for the <city,country> format
-			if (inputVal.includes(',')) {
-				// If the country code is invalid (ex. athens,grrrr), keep only the city name
-				if (inputVal.split(',')[1].length > 2) {
-					inputVal = inputVal.split(',')[0]
-
-					// Get the content from the existing city
-					content = cityName
-				} else {
-					// Country code is valid so keep both city and country
-					content = `${cityName},${cityCountry}`
-				}
-			} else {
-				// Only the <city> format is used
-				content = cityName
-			}
-
-			// Return whether or not the existing content matches the form input value
-			return content == inputVal.toLowerCase()
-		})
-
-		// If filteredArray is not blank, we have matches so we show a message and exit
-		if (filteredArray.length > 0) {
-			msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city__name").textContent} ...otherwise be more specific by providing the country code as well 😉`;
-
-			msg.classList.add('visible')
-
-			form.reset()
-			input.focus()
-
-			return
-		}
+const WeatherContainer = document.getElementById("weather-container");
+const url='https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}&units=metric';
+const API_KEY='eba6f122079354c1574c61520d9de148';
+const inputvalue = document.getElementById('input-data');
+let weatherArr = [];
+let weatherObjArr=[];
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	let cityname = inputvalue.value;
+	if(cityname === ''){
+		alert('please enter city name');
+		return;
 	}
-
-	// AJAX magic
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`
-
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			// If we get a 404 code, throw an error
-			if (data.cod == '404') {
-				throw new Error(`${data.cod}, ${data.message}`)
-			}
-
-			// Let's destructure the data object
-			const {main, name, sys, weather} = data
-
-			// Define the icon location
-			// const icon = `img/weather/${weather[0]['icon']}.svg`
-
-			// Create the list item for the new city
-			const li = document.createElement('li')
-
-			// Define markup
-			const markup = `
-			
-
-				<div>
-					<h2>${Math.round(main.temp)}<sup>°C</sup></h2>
-					<p class="city__conditions">${weather[0]['description'].toUpperCase()}</p>
-					<h3><span class="city__name">${name}</span><span class="city__country">${sys.country}</span></h3>
-				</div>
-			`
-
-			// Add the new markup to the list item
-			li.innerHTML = markup
-
-			// Add the new list item to the page
-			list.appendChild(li)
-		})
-		.catch(() => {
-			msg.textContent = 'Please search for a valid city!'
-			msg.classList.add('visible')
-		})
-
-	msg.textContent = ''
-
-	form.reset()
-	input.focus()
+	if(weatherArr.includes(cityname)){
+		alert(`${cityname.toUpperCase()} city name already exists!`);
+		return;
+	}
+	else{
+		weatherArr.push(cityname);
+		checkcityweather(cityname);
+	}
+	form.reset();
 })
-
-	// <figure>
-				// 	<img src="${icon}" alt="${weather[0]['description']}">
-				// </figure>
+async function checkcityweather(city){
+	try{
+		let result =await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+		let data = await result.json();
+    let icon_url = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let obj = {
+      cityName: data.name,
+      weather: data.weather[0].description,
+      temp: data.main.temp,
+      feel: data.main.feels_like,
+      humidity: data.main.humidity,
+      pressure: data.main.pressure,
+      wind: data.wind.speed,
+      clouds: data.clouds.all,
+      sys_sunRise: data.sys.sunrise,
+      sys_sunSet: data.sys.sunset,
+      img_url: icon_url,
+    };
+	weatherObjArr.push(obj);
+	weatherObjArr.sort((a,b)=>a['temp']-b['temp']);
+	weatherDetails(weatherObjArr);
+	}
+	catch (err) {
+		console.log(err);
+	  }
+}
+function weatherDetails(objArray) {
+	WeatherContainer.innerHTML = "";
+	objArray.forEach((element) => {
+	  const name = element.cityName;
+	  const weather = element.weather;
+	  const main = element.temp;
+	  const wind = element.wind;
+	  const clouds = element.clouds;
+	  const sys_sunRise = element.sys_sunRise;
+	  const img= element.img_url;
+	  const feel= element.feel;
+	  const humidity= element.humidity;
+	  const pressure= element.pressure;
+	  let sunRise_time = new Date(sys_sunRise * 1000);
+	  sunRise_time = `${sunRise_time.getHours()} : ${sunRise_time.getMinutes()} am`;
+	  let sys_sunSet = element.sys_sunSet;
+	  let sunSet_time = new Date(sys_sunSet * 1000);
+	  sunSet_time = `${sunSet_time.getHours()} : ${sunSet_time.getMinutes()} pm`;
+	  let weather_div = document.createElement("div");
+	  weather_div.className = "weather-div";
+	  weather_div.innerHTML = `<div id="temp_deg">
+								  <div>${main} <span>&#176;</span>C</div>
+								  <div><img src=${img}></div>
+							  </div>
+								  <div>
+									  <div id="extra-details">
+										<div id="feels-like"> 
+										  <div>${feel} <span>&#176;</span>C<sub> feels like</sub></div>
+										  <div>${weather} <sub>weather</sub></div>
+										</div>
+										<div id="wind-detail"><div>${wind} m/s <sub>Wind speed</sub></div><div>${humidity}% <sub>Humidity</sub></div><div>${pressure} hPa <sub>Air Pressure</sub></div></div>
+										<div id="cloudiness">Cloudiness ${clouds}%</div>
+									  </div>
+									  <div id="city-name-container">
+										  <div>
+										  <p id="city-name-p">${name.toUpperCase()}</p>
+										  </div>
+										  <div id="sun">
+											<p><span class=material-icons id="sunrise_icon">wb_sunny</span><span>${sunRise_time}</span></p>
+											<p><span class=material-icons id="sunset_icon">wb_twilight</span><span>${sunSet_time}</span></p>
+										  </div>
+									  </div>
+								  </div>
+							  `;
+							  WeatherContainer.appendChild(weather_div);
+	});
+  }
